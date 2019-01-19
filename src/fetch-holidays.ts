406de0d3352fa200fb07f8f2fa4c21a782ed0e21
Mitica <canteadumitru@gmail.com>
2019-01-19 12:55:46 +0200

    local ical parser

--- src/fetch-holidays.ts
+++ src/fetch-holidays.ts
@@ -1,22 +1,36 @@
 
-import * as got from 'got';
+import fetch from 'cross-fetch';
 
-const ical = require('ical');
+import ical from './ical';
 
 export function fetchHolidays(url: string, timeout?: number): Promise<{ [key: string]: IcalHoliday }> {
     timeout = timeout || 5000;
 
-    return got(url, {
+    const promise = fetch(url, {
         method: 'GET',
-        timeout,
     })
         .then(response => {
-            if (response.statusCode && response.statusCode >= 400) {
+            if (response.status && response.status >= 400) {
                 throw new Error("Bad response from server");
             }
-            return response.body;
+            return response.text();
         })
         .then(data => ical.parseICS(data));
+
+    return timeoutPromise(promise, timeout);
+}
+
+function timeoutPromise<T>(promise: Promise<T>, ms: number) {
+    const timeout = new Promise((_resolve, reject) => {
+        setTimeout(() => {
+            reject('Timed out in ' + ms + 'ms.')
+        }, ms)
+    });
+
+    return Promise.race([
+        promise,
+        timeout,
+    ]);
 }
 
 export type IcalHoliday = {
